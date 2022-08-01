b84361a88bb82bd2150a017c753198524ea36398
leath-dub <77889270+leath-dub@users.noreply.github.com>
2022-08-01 19:52:27 +0100

    first commit

---
+++ thm/gruvbox.h
@@ -0,0 +1,6 @@
+static const char* normbgcolor  = "#282828";
+static const char* normfgcolor  = "#ebdbb2";
+static const char* selbgcolor   = "#555555";
+static const char* selfgcolor   = "#ebdbb2";
+static const char* urgbgcolor   = "#111111";
+static const char* urgfgcolor   = "#cc0000";
